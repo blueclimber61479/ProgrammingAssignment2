248c842466ed74979cd8014b8ea0d9146c11cdec
blueclimber61479 <snowbird79@gmail.com>
2015-03-22 13:52:52 -0700

    homework edits

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(matrix) m <<- matrix
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
 
-
+cachemean <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
